a4e2aa812032979d06e3b7adae43d1a72f77bbd1
Annie Elizabeth Freeman <annie.e.freeman@gmail.com>
2020-01-25 12:43:59 +0100

    Complete Taum and b'day

---
+++ taum-bday.py
@@ -0,0 +1,62 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'taumBday' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER b
+#  2. INTEGER w
+#  3. INTEGER bc
+#  4. INTEGER wc
+#  5. INTEGER z
+#
+
+def taumBday(b, w, bc, wc, z):
+    # Write your code here
+    if bc > (wc + z):
+       #convert white to black
+        total = (w * wc) + (b * (wc + z))
+
+    elif wc > (bc + z):
+        #convert black to white
+        total = (b * bc) + (w * (bc + z))
+
+    else:
+        #do normal calculation
+        total = (b * bc) + (w * wc)
+
+
+    return total
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        b = int(first_multiple_input[0])
+
+        w = int(first_multiple_input[1])
+
+        second_multiple_input = input().rstrip().split()
+
+        bc = int(second_multiple_input[0])
+
+        wc = int(second_multiple_input[1])
+
+        z = int(second_multiple_input[2])
+
+        result = taumBday(b, w, bc, wc, z)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
