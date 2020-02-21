850181d4fafa433aebb15047f2375b781141ef70
Annie Elizabeth Freeman <annie.e.freeman@gmail.com>
2020-02-21 16:26:06 +0100

    Complete delete a node from a linked list

---
+++ delete-node-linked-list.py
@@ -0,0 +1,66 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the deleteNode function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def deleteNode(head, position):
+    current = head
+    position_counter = 0
+    if position != 0:
+        while position_counter < position - 1:
+            current = current.next
+            position_counter += 1
+
+        pre_delete = current
+        current = current.next
+
+        pre_delete.next = current.next
+    else:
+        head = current.next
+
+    return head
+
+if __name__ == '__main__':
