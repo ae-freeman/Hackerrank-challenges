49485bb49f466b8ba7b3e9f7b6e88d54d6ab406e
Annie Elizabeth Freeman <annie.e.freeman@gmail.com>
2020-02-25 22:48:10 +0100

    Complete delete duplicate nodes from a sorted singly linked list

---
+++ delete-duplicate-nodes-sorted-linked-list.py
@@ -0,0 +1,63 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the removeDuplicates function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def removeDuplicates(head):
+    if head is not None:
+        current = head
+
+        while current.next != None:
+            next_node = current.next
+            if current.data == next_node.data:
+                current.next = next_node.next
+            else:
+                current = current.next
+
+    return head
+
+
+if __name__ == '__main__':
